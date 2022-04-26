cfb1f922c54a0650d85a140fca867ab5ac50bddd
buben <nati165@onet.eu>
2022-04-26 21:43:46 +0200

    update buy-sell template

--- helpers/generate_png_files.py
+++ helpers/generate_png_files.py
@@ -7,6 +7,7 @@ import pysvg.structure
 class GeneratePNGFiles:
     ACTIONS_TEMPLATE_PATH = "./../graphics/actions_template"
     CHARACTERS_PATH = "./../graphics/characters"
+    ITEM_PATH = "./../graphics/objects"
 
     #TODO check if works after refactor
     def generate_png_one_character(self, action, character):
@@ -44,7 +45,6 @@ class GeneratePNGFiles:
 
     def generate_png_two_character_one_item(self, action, f_character, s_character, item):
         #TODO check if file exists
-        #TODO add item
         # bb = self.check_if_action_two_exist(action, f_character, s_character)
         f_character_folder_path = f"{self.CHARACTERS_PATH}/{f_character}"
         template = pyp.parse(f"{self.ACTIONS_TEMPLATE_PATH}/{action}.svg")
@@ -52,6 +52,7 @@ class GeneratePNGFiles:
 
         self.paste_character_parts_to_template(f_character, phases_amount, template, "f_character")
         self.paste_character_parts_to_template(s_character, phases_amount, template, "s_character")
+        self.paste_item_to_template(item, phases_amount, template)
 
         new_action_path = f"{f_character_folder_path}/{f_character}_{s_character}_{item}_{action}"
         template.save(f"{new_action_path}.svg")
@@ -92,6 +93,23 @@ class GeneratePNGFiles:
                 for j in range(part_group_number):
                     place_to_paste.addElement(part_to_paste.getElementAt(j))
 
+    def paste_item_to_template(self, item, phases_amount, template):
+        item_svg = pyp.parse(f"{self.ITEM_PATH}/{item}.svg")
+        item_to_paste = item_svg.getElementAt(1).getElementAt(0)
+        item_group_number = len(item_to_paste._subElements)
+
+        for i in range(1, phases_amount):
+            phase = template.getElementAt(i)
+            place_to_paste = phase.getElementByID("item")[0].getElementAt(0)
+
+            try:
+                place_to_paste.getAttributes()["transform"] = item_to_paste.getAttributes()["transform"]
+            except KeyError:
+                print(f"Item {item} has no own transform to paste to template file")
+
+            for j in range(item_group_number):
+                place_to_paste.addElement(item_to_paste.getElementAt(j))
+
     def get_character_parts(self, character, folder_path):
         list_file = open(f"{folder_path}/{character}_parts.txt", "r")
         parts_list = list_file.read().splitlines()
@@ -101,6 +119,6 @@ class GeneratePNGFiles:
 
 
 # GeneratePNGFiles().generate_png_one_character("walking", "merchant")
-# GeneratePNGFiles().generate_png_two_character_one_item("buy-sell", "main_hero", "common_man", "sign")
+GeneratePNGFiles().generate_png_two_character_one_item("buy-sell 2", "main_hero", "main_hero", "flakon")
 # GeneratePNGFiles().generate_png_two_character("fight_running", "main_hero", "common_man")
-GeneratePNGFiles().generate_png_two_character("fight_death", "main_hero", "main_hero")
+# GeneratePNGFiles().generate_png_two_character("fight_death", "main_hero", "main_hero")
